dba61983dd63f26ccd76537f53c21232968bfb90
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-11 18:51:03 +0300

    add PrintArray

---
+++ Ex01_2D_Array/Program.cs
@@ -0,0 +1,43 @@
+﻿// Создаем двумерный массив
+
+string[,] table = new string[2,5];
+// table[1,2] = "слово";
+// for(int rows = 0; rows < 2; rows++)
+// {
+//     for(int columns = 0; columns < 5; columns++)
+//     {
+//         //Console.WriteLine($"{columns}");
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+// for(int i = 0; i < matrix.GetLength(0); i++) // matrix.GetLength(0) возвращает кол-во строк
+// {
+//     for(int j = 0; j < matrix.GetLength(1); j++) // matrix.GetLength(1) возвращает кол-во столбцов
+//     {
+//         //Console.WriteLine($"{columns}");
+//         Console.Write($"{matrix[i, j]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+void PrintArray(int[,] matr);
+{
+    for(int i = 0; i < matr.GetLength(0); i++) // matrix.GetLength(0) возвращает кол-во строк
+    {
+        for(int j = 0; j < matr.GetLength(1); j++) // matrix.GetLength(1) возвращает кол-во столбцов
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] matrix = new int[3,4];
+
+PrintArray(matrix);
+
+//Console.WriteLine("Hello, World!");

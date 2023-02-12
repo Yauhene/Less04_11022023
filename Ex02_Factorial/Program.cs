19892c825fc9ffec14cc2519c9d49ac6129ed09b
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-02-12 11:05:44 +0300

    problem in Fibonacci

--- Ex02_Factorial/Program.cs
+++ Ex02_Factorial/Program.cs
@@ -1,5 +1,14 @@
 ﻿// Функция вычисления факториала
 
+// int Factorial(int n) 
+// {
+//     // 1! = 1; 0! = 1;
+//      if(n == 1) return 1;
+//      else return n * Factorial(n-1); 
+// }
+//       Console.WriteLine(Factorial(3)); // 1 * 2 * 3 = 6
+
+
 int Factorial(int n) 
 {
     // 1! = 1; 0! = 1;
@@ -7,5 +16,4 @@ int Factorial(int n)
      else return n * Factorial(n-1); 
  }
       Console.WriteLine(Factorial(3)); // 1 * 2 * 3 = 6
-
-
+      
---
+++ Ex03_Fibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// f(1) = 1 
+// f(2) = 1 
+// f(n) = f(n-1) + f(n-2) 
+int Fibonacci(int n)
+{ 
+    if(n == 1 || n == 2) return 1; 
+    //else Fibonacci(n-1) + Fibonacci(n-2); 
+    else Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 10; i++) 
+{
+     Console.WriteLine(Fibonacci(i)); 
+}
